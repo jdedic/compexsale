26347a691ade6ead7a0136c2d9755b5128424d08
Jovana Dedic <jovana.dedic@inform-technologies.com>
2021-12-01 14:49:57 +0100

    create user - change password

---
+++ RetailPlatform.API/wwwroot/js/custom-js/create-user.js
@@ -0,0 +1,5 @@
+﻿function changePassword() {
+    document.getElementById("changePasswordBtn").classList.add("d-none"); 
+    document.getElementById("userPassword").classList.remove("d-none"); 
+    document.getElementById("repeatUserPassword").classList.remove("d-none"); 
+}
