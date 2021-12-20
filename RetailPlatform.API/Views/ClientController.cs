58f8366cbbaff6ac815afcf193bb1012c256fcaa
Jovana Dedic <jovana.dedic@inform-technologies.com>
2021-12-20 08:38:25 +0100

    about us page

---
+++ RetailPlatform.API/Views/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetailPlatform.API.Views
+{
+    public class ClientController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
