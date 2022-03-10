e492262d677ad0b6cf604381b0d57170923abdd4
Jovana Dedic <jovana.dedic@inform-technologies.com>
2022-03-10 11:08:52 +0100

    add requests to home page

--- RetailPlatform.Common/Interfaces/Service/IAddService.cs
+++ RetailPlatform.Common/Interfaces/Service/IAddService.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using RetailPlatform.Common.Entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RetailPlatform.Common.Interfaces.Service
